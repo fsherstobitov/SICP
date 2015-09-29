faa19964282d512c2c791b6d7c7930ef57ab8247
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-09-29 23:01:53 +0500

    add some tests to watch the results of middle procedures

--- ch1/ex3.scm
+++ ch1/ex3.scm
@@ -1,6 +1,10 @@
 (define (square x) (* x x))
 
+(square 4)
+
 (define (sum-of-squares x y) (+ (square x) (square y)))
+
+(sum-of-squares 4 5)
 
 (define (sum-of-squares-of-two-biggest x y z)
 	(cond (and (< x y) (< x z)) (sum-of-squares y z)
